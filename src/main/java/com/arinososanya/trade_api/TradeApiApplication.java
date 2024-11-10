27d8b18ee2749e0af3ab2c840ee526d553497fad
Arin Ososanya <arin.ososanya@gmail.com>
2024-11-10 19:53:25 +0000

    Updated my folder structure

---
+++ src/main/java/com/arinososanya/trade_api/TradeApiApplication.java
@@ -0,0 +1,14 @@
+package com.arinososanya.trade_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// This file is my main application class. It's the entry point of my SpringBoot application.
+public class TradeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arinososanya/trade_api/TradeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arinososanya.trade_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
